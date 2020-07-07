e49c90acb1022278ac116c5a7febfdb28799b919
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-07-07 21:03:32 +0530

    A code to run sql queries using sqlite

---
+++ Web Apps/Learning Flask/sqlite_test.py
@@ -0,0 +1,11 @@
+# 1.import sqlite
+import sqlite3
+
+# 2. create a connection to DB     
+conn = sqlite3.connect('todo.db')
+
+# 3. Write your sql query   
+query = "select 'HELLO';"
+
+# 4. execute the query
+result = conn.execute(query)
